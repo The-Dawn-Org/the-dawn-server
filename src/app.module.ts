a8b5e68ad6ba810f0b2d35eab5c9d7ec6f653116
Amit Peleg <u9910905@bsmch.net>
2026-09-09 15:49:08 +0300

    feat(several): implement mock dayily-interceptions endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from "@nestjs/common";
+import { RouterModule } from "@nestjs/core";
 import { FinanceModule } from "./modules/finance/finance.module.js";
 import { StatisticsModule } from "./modules/statistics/statistics.module.js";
 
 @Module({
-  imports: [StatisticsModule, FinanceModule],
+  imports: [
+    StatisticsModule,
+    FinanceModule,
+    RouterModule.register([
+      {
+        path: "finance",
+        module: FinanceModule,
+      },
+    ]),
+  ],
 })
 export class AppModule {}
