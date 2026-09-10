c687c78bb982691886006401877a94e7c62208f3
amitai <u9866700@bsmch.net>
2026-09-10 04:41:52 -0700

    fix(database.module.ts): change back db host to localhost

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -23,7 +23,7 @@ export const DB_CONNECTION = Symbol("DB_CONNECTION");
       useFactory: async (dbStatus: DbStatusService): Promise<DataSource> => {
         const dataSource = new DataSource({
           type: "postgres",
-          host: process.env.DB_HOST || "d",
+          host: process.env.DB_HOST || "localhost",
           port: Number(process.env.DB_PORT) || 5432,
           username: process.env.DB_USERNAME || "postgres",
           password: process.env.DB_PASSWORD || "postgres",
