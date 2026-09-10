9122c3afa01f605e4e79893990615725721d3bde
Arbel Nachshon <u9839939@bsmch.net>
2026-09-10 16:13:10 +0300

    feat(several): add env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from "@nestjs/common";
 import { RouterModule } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { AiModule } from "./modules/AIAnalysis/ai.module.js";
 import { DatabaseModule } from "./modules/database/database.module.js";
 import { EventsModule } from "./modules/events/events.module.js";
 import { FinanceModule } from "./modules/finance/finance.module.js";
 import { StatisticsModule } from "./modules/statistics/statistics.module.js";
-import { AiModule } from "./modules/AIAnalysis/ai.module.js";
 
 @Module({
   imports: [
